74150258406f414c5fa61888f0024391e186a97d
palutz <stefano.paluello@gmail.com>
2018-09-28 17:19:52 +0100

    myVector: add equals, hash and init case with only one value

--- spMath/Program.fs
+++ spMath/Program.fs
@@ -4,8 +4,19 @@ type spVector (c : float list) =
   let vect =
     match c with 
     | [] -> [0.0; 0.0]
+    | [x] -> [x; 0.0]
     | _ -> c
 
+  member this.ToList () =
+    vect
+
+  override x.GetHashCode() =
+    hash (vect)
+
+  override x.Equals y =
+    match y with
+    | :? spVector as v -> v.ToList() = vect
+    | _ -> false
 
   override x.ToString () = sprintf "Vector: %A" vect
 
@@ -13,5 +24,11 @@ type spVector (c : float list) =
 [<EntryPoint>]
 let main argv =
   let sv = spVector [1.;2.]
-  sv.ToString() |> printfn "%s"
+  let sv1 = spVector [1.;2.]
+  let sv2 = spVector [1.]
+  if sv = sv1 then "true" else "false"
+  |> printfn "sv and sv1 %s"
+  if sv = sv2 then "true" else "false"
+  |> printfn "sv and sv2 %s"
+  printfn "sv %s, sv2 %s" (sv.ToString()) (sv2.ToString())
   0 // return an integer exit code
